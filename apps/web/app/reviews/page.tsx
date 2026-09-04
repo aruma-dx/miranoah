c24159befaf5021d485a36181a916ec22180ccc1
aruma-dx <dx@cozoru.com>
2026-09-04 18:04:53 +0900

    Create page.tsx

---
+++ apps/web/app/reviews/page.tsx
@@ -0,0 +1,22 @@
+import AIReviewPage from (
+  "../components/AIReviewPage"
+);
+
+
+export const dynamic =
+  "force-dynamic";
+
+
+export default function Page() {
+  const apiBaseUrl =
+    process.env.API_BASE_URL
+    ?? "";
+
+  return (
+    <AIReviewPage
+      apiBaseUrl={
+        apiBaseUrl
+      }
+    />
+  );
+}
